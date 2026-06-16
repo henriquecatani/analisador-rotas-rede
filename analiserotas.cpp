cb4c6bcb1d0df31794497fa96e9d024d944b0bb6
MateusRW-br <mateus.roque2007@gmail.com>
2026-06-16 19:55:55 -0300

    Adicionando funcao encontraMenorCaminho e mudancas no graph

--- analiserotas.cpp
+++ analiserotas.cpp
@@ -4,6 +4,8 @@
 #include "parser.cpp"
 using namespace std;
 
+string filename;
+
 // Commit fazendo um esboço de como sera o output do terminal
 int main(int argc, char* argv[]){
     if (argc < 2) {
@@ -12,12 +14,14 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
+    filename = argv[1];
+
     graph::digraph graph;
     if (!readLog(argv[1], graph)) {
         cerr << "Erro ao ler arquivo " << argv[1] << std::endl;
         return 1;
     }
-
+    
     cout << "Grafo de roteamento inicializado!" << endl;
     cout << "Vértices únicos (IPS) " << graph.size() << " | " << "Arestas: " << "graph.num_links()" << endl;
 
@@ -33,16 +37,12 @@ int main(int argc, char* argv[]){
         cout << "================================================" << endl;
         cin >> opcao;
 
-        int origem, destino;
         switch (opcao){
             case 1:
-                exportarGrafo("grafo_saida", graph);
+                exportarGrafo(graph);
                 break;
             case 2:
-                cout << "Digite o IP de Origem: ";
-                cin >> origem;
-                cout << "Digite o IP de Destino: ";
-                cin >> destino;
+                encontraMenorCaminho(graph);
                 //Fazer encontrar menor caminho
             case 3:
                 //Fazer calcular o diâmetro do grafo
@@ -56,7 +56,7 @@ int main(int argc, char* argv[]){
 
 }
 
-void exportarGrafo(const std::string& filename, graph::digraph& graph) {
+void exportarGrafo(graph::digraph& graph, const std::optional<std::vector<std::string>> &path_vector = std::nullopt) {
     cout << "Selecione o formato de saída do Graphviz:" << endl;
     cout << "1. Tela" << endl;
     cout << "2. Imagem (PNG)" << endl;
@@ -65,19 +65,32 @@ void exportarGrafo(const std::string& filename, graph::digraph& graph) {
     int opcao;
     cout << "Opção: ";
     cin >> opcao;
-
+    
     switch (opcao)
     {
     case 1:
-        graph.draw(graph::digraph::output_format::SCREEN, filename);
+        graph.draw(graph::digraph::output_format::SCREEN, filename, path_vector);
         break;
     case 2:
-        graph.draw(graph::digraph::output_format::PNG, filename);
+        graph.draw(graph::digraph::output_format::PNG, filename, path_vector);
         break;
     case 3:
-        graph.draw(graph::digraph::output_format::PDF, filename);
+        graph.draw(graph::digraph::output_format::PDF, filename, path_vector);
         break;
     default:
         break;
     }
+}
+void encontraMenorCaminho(graph::digraph& graph){
+    string origem, destino;
+
+    cout << "Digite o IP de Origem: ";
+    cin >> origem;
+    cout << "Digite o IP de Destino: ";
+    cin >> destino;
+    vector<string> vetorMenorCaminho;
+    vetorMenorCaminho = graph.shortest_path(origem, destino);
+    exportarGrafo(graph, vetorMenorCaminho);
+    // Fazer interface temrinal
+    
 }
